59e1e7c57f4265fb4aeb71245da289b6da481c57
favour ayorinde <Favourayorinde@gmail.com>
2024-09-09 12:01:29 +0100

    Adding instructions

--- src/App.jsx
+++ src/App.jsx
@@ -6,58 +6,65 @@ import Dropdown from './assets/Components/Dropdown'
 import Shadows from './assets/Components/Shadows'
 import { useState } from 'react'
 
+//In order to make this work when deployed, use `${import.meta.env.BASE_URL}` and also import your own environment files.
+
 function App() {
 
   const [selectedOption, setSelectedOption] = useState('')
   const [customColors, setcustomColors] = useState({
-    BodyHull : "wheat",
+    BodyHull: "wheat",
     MiddleRim: "wheat",
-    Grill : "white",
-    BackPanel : "orange",
-    KnobRim : "gray",
-    Knobs : "wheat",
-    RodHandle : "orange",
-    RubberHandle : "wheat",
-    HandlePadding : "orange",
-    Band : "orange",
-    KnobMarker : "orange",
-    Screen : "cream",
-    DisplayText : "black",})
+    Grill: "white",
+    BackPanel: "orange",
+    KnobRim: "gray",
+    Knobs: "wheat",
+    RodHandle: "orange",
+    RubberHandle: "wheat",
+    HandlePadding: "orange",
+    Band: "orange",
+    KnobMarker: "orange",
+    Screen: "cream",
+    DisplayText: "black",
+  })
 
   const handleColors = (e) => {
-    setcustomColors({...customColors, [e.target.name] : e.target.value})
+    setcustomColors({ ...customColors, [e.target.name]: e.target.value })
   }
 
   console.log(customColors)
 
   return (
     <div className="App">
-        <Canvas
-          shadows
-          
-          className='canvas'
-          camera={{ position: [20, 5, 20], fov: 8 }}>
-          <color attach="background" args={['#fff']} />
-          <fog attach="fog" args={['#FFF', 30, 75]} />
-          <Environment files='forest.exr'/>
-          <directionalLight position={[-2, 10, 5]} intensity={2} />
+      <Canvas
+        shadows
 
-          <Model customColors = {customColors}/>
-          <Circle rotation={[-Math.PI / 2, 0, 0]}position={[0, -0.1, 0]} args={[100, 100]} color={'#FFF'}/>
-          <OrbitControls target={[0, 1, 0]} minPolarAngle={0.31} maxPolarAngle={Math.PI / 2 - 0.09}/>
-          <Shadows />
-        </Canvas>
-        <div className='settings'>
-          <Dropdown
-          selectedOption={selectedOption} setSelectedOption={setSelectedOption}/>
-          <div className="colorwrapper">
-            <p>Change Color</p>
+        className='canvas'
+        camera={{ position: [20, 5, 20], fov: 8 }}>
+        <color attach="background" args={['#fff']} />
+        <fog attach="fog" args={['#FFF', 30, 75]} />
+        <Environment files='forest.exr' />
+        <directionalLight position={[-2, 10, 5]} intensity={2} />
+
+        <Model customColors={customColors} />
+        <Circle rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.1, 0]} args={[100, 100]} color={'#FFF'} />
+        <OrbitControls target={[0, 1, 0]} minPolarAngle={0.31} maxPolarAngle={Math.PI / 2 - 0.09} />
+        <Shadows />
+      </Canvas>
+      <div className='settings'>
+        <Dropdown
+          selectedOption={selectedOption} setSelectedOption={setSelectedOption} />
+        <div className="colorwrapper">
+          <p>Change Color</p>
           <input
             name={selectedOption}
             value={customColors[selectedOption]}
-            className= 'colorchange' type='color'
+            className='colorchange' type='color'
             onChange={handleColors} /></div>
-        </div>
+          <p style={{textAlign: "center"}}>Select a part from the dropdown and use the color picker to change the color!</p>
+          <p>Hold left mouse button to orbit</p>
+          <p>Hold right mouse button to pan</p>
+          <p>use the scroll wheel to zoom</p>
+      </div>
     </div>
   )
 }
