eab323b105258d55cd183bccc61ae0eb42b0cf9f
favour ayorinde <Favourayorinde@gmail.com>
2024-09-09 01:56:28 +0100

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/vintage-radio/';
+  return {
+    base,
   plugins: [react()],
-  base: '/vintage-radio'
+  }
 })
